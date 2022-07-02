a9c46d142fba14fd743005edde1db7caf11fc8b6
oneqiao <1494153344@qq.com>
2022-07-02 11:00:22 +0800

    master

--- src/main.js
+++ src/main.js
@@ -8,9 +8,10 @@ import 'element-ui/lib/theme-chalk/index.css'
 
 var axios = require('axios')
 axios.defaults.baseURL = 'http://localhost:8443/api'
-
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
+
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
